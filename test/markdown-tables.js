cfed9b9965aa0072eada5e6aaf1bf068e398fdfc
Matt Jarrett <cujarrett@gmail.com>
2021-07-21 20:31:14 -0500

    fix: migrate to ava for tests

---
+++ test/markdown-tables.js
@@ -0,0 +1,60 @@
+const test = require("ava")
+const markdownTables = require("../src/index.js")
+
+test("Unit - getInput", async (assert) => {
+  const expected = [{
+    "Header 1": "Some data",
+    "Header-2": "frogs",
+    "“Header 3”": "Joe’s Fish",
+    "‘Header 4’": "Part",
+    "Header-2_1": "Pina"
+  }, {
+    "Header 1": "Oil",
+    "Header-2": "“Yellow Tuna”",
+    "‘Header 4’": "Work, Work, Work",
+    "Header-2_1": "Yellow"
+  }, {
+    "Header 1": 11,
+    "Header-2": "Header-2",
+    "“Header 3”": "Sharks;Birds;People",
+    "‘Header 4’": "Great",
+    "Header-2_1": "Gifs make me smile"
+  }]
+
+  const actual = await markdownTables.getInput("./test/data/normal-test-input.xlsx")
+  assert.deepEqual(actual, expected)
+})
+
+test("Unit - getColumns", async (assert) => {
+  const mockInput = [{
+    red: 1,
+    blue: 2,
+    green: 3
+  }, {
+    red: 4,
+    blue: 5,
+    green: 6
+  }]
+
+  const expected = [["red", 1, 4], ["blue", 2, 5], ["green", 3, 6]]
+  const actual = markdownTables.getColumns(mockInput)
+  assert.deepEqual(actual, expected)
+})
+
+test("Unit - getColumnWidth", async (assert) => {
+  const expected = 6
+  const actual = markdownTables.getColumnWidth(["a", "bubble", "c"])
+  assert.deepEqual(actual, expected)
+})
+
+test("Unit - getAllColumnWidths", async (assert) => {
+  const expected = [1, 3]
+  const actual = markdownTables.getAllColumnWidths([["a", "b", "c"], ["d", "two"]])
+  assert.deepEqual(actual, expected)
+})
+
+test("Unit - getHeaderLineBreak", async (assert) => {
+  const expected = "|---|-------|---|\n"
+  const actual = markdownTables.getHeaderLineBreak([1, 5, 1])
+  assert.deepEqual(actual, expected)
+})
